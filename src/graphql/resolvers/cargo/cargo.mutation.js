d993d04bf4214c277f15b1234a22336e8f8f4a7a
Misa <misa.cc2393293@gmail.com>
2022-07-06 22:02:45 -0500

    cargos completo

--- src/graphql/resolvers/cargo/cargo.mutation.js
+++ src/graphql/resolvers/cargo/cargo.mutation.js
@@ -7,6 +7,18 @@ const resolvers = {
             } catch (error) {
                 throw new Error(error.message)
             }
+        },
+        async deleteCargo(_, { id_cargo }, { models }) {
+            try {
+                await models.cargos.destroy({
+                    where: {
+                        id_cargo
+                    }
+                })
+                return true
+            } catch (error) {
+                throw new Error(error.message)
+            }
         }
     }
 }
