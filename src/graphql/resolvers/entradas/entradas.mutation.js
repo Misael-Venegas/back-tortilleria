57c5a96eadc22e0a3e1b3e267f57483853d71d20
Misa <misa.cc2393293@gmail.com>
2022-08-01 18:55:48 -0500

    fechas

--- src/graphql/resolvers/entradas/entradas.mutation.js
+++ src/graphql/resolvers/entradas/entradas.mutation.js
@@ -4,18 +4,26 @@ const resolvers = {
       const { cantidad, id_tipo_almacen, id_proveedor, fechaRegistro, id_almacen } = input
 
       try {
-
         const obtenerAlmacen = await models.almacen.findByPk(id_almacen)
-        console.log(obtenerAlmacen.cantidadTotal, obtenerAlmacen.nombreProducto)
-        /*
-                await models.entrada.create(
-                  cantidad,
-                  id_tipo_almacen,
-                  id_proveedor,
-                  fechaRegistro,
-                  id_almacen
-                )
-        */
+        const sumaCantidades = parseInt(obtenerAlmacen.cantidadTotal + cantidad);
+        await models.almacen.update({
+          cantidadTotal: sumaCantidades
+        }, {
+          where: {
+            id_almacen: obtenerAlmacen.id_almacen
+          }
+        })
+
+        await models.entrada.create(
+          {
+            cantidad,
+            id_tipo_almacen,
+            id_proveedor,
+            fechaRegistro,
+            id_almacen
+          }
+        )
+
         return true;
       } catch (error) {
         throw new Error(error.message);
--- src/graphql/resolvers/entradas/entradas.query.js
+++ src/graphql/resolvers/entradas/entradas.query.js
@@ -1,27 +1,17 @@
 const resolvers = {
   Query: {
-    async getEntradas(root, args, { models }) {
+    async getEntradas(_, { fecha }, { models }) {
       try {
         const [results, metadata] = await models.sequelize.query(`
-                SELECT 
-                  ea.id_entradas_almacen, 
-                  ea.cantidad, 
-                  e.id_entrada,
-                  e.Fecha fecha,
-                  i.descripcion, 
-                  i.id_insumos,
-                  p.nombre,
-                  p.id_proveedor,
-                  ta.nombre tipo_almacen,
-                  a.id_almacen
-                  from entradasalmacens ea
-                  INNER JOIN entradas e ON ea.id_entradas_almacen=e.id_entrada
-                  INNER JOIN almacens a ON ea.id_almacen=a.id_almacen
-                  INNER JOIN insumos i ON a.id_insumos=i.id_insumos
-                  INNER JOIN tipoalmacens ta ON i.id_tipo_almacen=ta.id_tipo_almacen
-                  INNER JOIN proveedors p ON e.id_proveedor=p.id_proveedor
-                `);
-        return results;
+                SELECT e.id_entrada, e.cantidad, e.fechaRegistro , al.nombreProducto, 
+                pr.nombre noProveedor , ta.nombre noTipoAlmacen FROM entradas e 
+                INNER JOIN almacens al ON al.id_almacen = e.id_almacen 
+                INNER JOIN proveedors pr on pr.id_proveedor = e.id_proveedor 
+                INNER JOIN tipoalmacens ta ON ta.id_tipo_almacen = e.id_tipo_almacen 
+                where e.fechaRegistro = '${fecha}'  
+                `
+        );
+        return results
       } catch (error) {
         throw new Error(error.message);
       }
