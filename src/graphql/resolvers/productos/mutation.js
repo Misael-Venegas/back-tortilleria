9d39d3258091dfc543df0ad913c954ac280db2a4
Misa <misa.cc2393293@gmail.com>
2022-08-23 21:21:08 -0500

    productos

--- src/graphql/resolvers/productos/mutation.js
+++ src/graphql/resolvers/productos/mutation.js
@@ -2,13 +2,25 @@
 const resolvers = {
     Mutation: {
         async agregarProducto(_, { nombreProducto }, { models }) {
+
+
             try {
+                const existeProducto = await models.productos.findAll({
+                    where: {
+                        nombre: nombreProducto
+                    }
+                })
+
+                if (existeProducto.length > 0) {
+                    throw new Error("El nombre del producto ya existe")
+                }
+
                 await models.productos.create({
                     nombre: nombreProducto
                 })
                 return true
             } catch (error) {
-                throw new Error("Error al intenatar agregar un nuevo producto " + error.message)
+                throw new Error(error.message)
             }
         },
         async eliminarProducto(_, { id_producto }, { models }) {
@@ -23,7 +35,7 @@ const resolvers = {
                 throw new Error("No se puedo completar la operación: " + error.message);
             }
         },
- 
+
     }
 }
 module.exports = resolvers
